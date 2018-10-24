f748aa61a28e53ec5ec88fc3fcc96f9868dbeaab
Benjamin Urquhart <24701883+BenjaminUrquhart@users.noreply.github.com>
2018-10-23 21:09:38 -0400

    Added Reddit route

--- src/main/java/net/explodingbush/ksoftapi/enums/Routes.java
+++ src/main/java/net/explodingbush/ksoftapi/enums/Routes.java
@@ -1,4 +1,14 @@
 package net.explodingbush.ksoftapi.enums;
 
 public enum Routes {
+  REDDIT_URL("https://api.ksoft.si/meme/");
+  
+  private final route;
+  
+  private Routes(String route){
+    this.route = route;
+  }
+  public String toString(){
+    return this.route;
+  }
 }

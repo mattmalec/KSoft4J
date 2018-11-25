cfa4350ae8e5f6b46540b9b5aacf73a92c66b60b
Benjamin Urquhart <24701883+BenjaminUrquhart@users.noreply.github.com>
2018-11-24 20:36:20 -0500

    Third time I'm adding isNSFW() Matt

--- src/main/java/net/explodingbush/ksoftapi/enums/ImageTag.java
+++ src/main/java/net/explodingbush/ksoftapi/enums/ImageTag.java
@@ -1,5 +1,12 @@
 package net.explodingbush.ksoftapi.enums;
 
 public enum ImageTag {
-    PEPE, DOGE, KAPPA, DAB, BIRB, DOG, FLOOFS, FBI, GLOMP, LICK, KISS, SMACK, SPANK, PAT, HUG, HENTAI_GIF, LEWD, NEKO
+    PEPE, DOGE, KAPPA, DAB, BIRB, DOG, FLOOFS, FBI, GLOMP, LICK, KISS, SMACK, SPANK, PAT, HUG, HENTAI_GIF, LEWD, NEKO, CODE;
+
+	public boolean isNSFW() {
+		return isNSFW(this);
+	}
+	public boolean isNSFW(ImageTag tag) {
+		return tag.equals(HENTAI_GIF) || tag.equals(NEKO) || tag.equals(LICK) || tag.equals(LEWD);
+	}
 }

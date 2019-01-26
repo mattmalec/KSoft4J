22d20952ebb49fa8a27bfaa6fd513623b90d3803
BuildTools <unconfigured@null.spigotmc.org>
2019-01-26 13:11:50 -0500

    Deprecated ImageTag#CODE

--- src/main/java/net/explodingbush/ksoftapi/enums/ImageTag.java
+++ src/main/java/net/explodingbush/ksoftapi/enums/ImageTag.java
@@ -3,7 +3,9 @@ package net.explodingbush.ksoftapi.enums;
 import java.util.Arrays;
 
 public enum ImageTag {
-    PEPE, DOGE, KAPPA, DAB, BIRB, DOG, FLOOFS, FBI, GLOMP, LICK, KISS, SMACK, SPANK, PAT, HUG, HENTAI_GIF, LEWD, NEKO, CODE;
+    PEPE, DOGE, KAPPA, DAB, BIRB, DOG, FLOOFS, FBI, GLOMP, LICK, KISS, SMACK, SPANK, PAT, HUG, HENTAI_GIF, LEWD, NEKO, 
+    @Deprecated
+    CODE;
 	
 	public boolean isNSFW(ImageTag tag) {
 		return Arrays.asList(NEKO, HENTAI_GIF, LEWD, LICK).contains(tag);
